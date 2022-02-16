90ec75c888fdaab1ca149b18d02b710d66a9c466
Samarth9898 <samarth98ladani@gmail.com>
2022-02-16 10:56:11 +0530

    role list

---
+++ src/main/java/com/controller/RoleController.java
@@ -0,0 +1,48 @@
+package com.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bean.RoleBean;
+import com.dao.RoleDao;
+
+@Controller
+public class RoleController {
+
+	@Autowired
+	RoleDao roleDao;
+	
+	
+//	@RequestMapping(value ="newrole",method = RequestMethod.GET)
+	@GetMapping("/newrole")
+	public String newRole() {
+		return "Role";// jsp
+	}
+
+	@PostMapping("/saverole")
+	public String saveRole(RoleBean role) {
+		System.out.println(role.getRoleName());
+
+
+		roleDao.insertRole(role);
+		
+		return "Login";
+	}
+	
+	@GetMapping("/listroles")
+	public String listRoles(Model model) {
+
+		List<RoleBean> roles = roleDao.getAllRoles(); 
+		model.addAttribute("roles",roles);
+		return "ListRoles";
+	}
+	
+	
+}
